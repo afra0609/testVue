9c54c48c0c1e9b1f17b810546d1c96d43e42e70d
afra <afra0609@gmail.com>
2019-08-16 16:29:55 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{data}}</h1>
+        <test></test>
+        <ve-line :data="chartData"></ve-line>
+        <div class="btn" v-on:click="changeData">Change</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import test from './components/test'
+  import VeLine from 'v-charts/lib/line.common'
+  
 export default {
   name: 'app',
+  data(){
+    return{
+      data:"Charlotte's web",
+      chartData: {
+          columns: ['日期', '访问用户', '下单用户', '下单率'],
+          rows: [
+            { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
+            { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
+            { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
+            { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
+            { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
+            { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+          ]
+        }
+    }
+  },
   components: {
-    HelloWorld
+    test,
+    VeLine
+  },
+  methods:{
+    changeData:function(){
+      console.log("push it!")
+      this.chartData.rows[0].访问用户 += 100
+    }
+
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="test">
+       <h1>hello</h1>
+   </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:"hey"
+        }
+    }
+}
+</script>>
+<style>
+    .test{
+        color:skyblue
+    }
+</style>>
